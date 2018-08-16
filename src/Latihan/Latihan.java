68cab37755a4ca1a74c4da6b2183a451213bb67d
user <user@192.168.0.12>
2018-08-16 20:40:20 +0700

    Tambah Konversi

---
+++ src/Latihan/Latihan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan {
+    public static void main(String[] args) {
+        int x=5;
+        for(int i=0;i<=x++;++i){
+            System.out.println(i+""+x+"");
+            
+        }
+        
+    }
+}
---
+++ src/Tugas/PerulanganBersarang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author user
+ */
+public class PerulanganBersarang {
+    public static void main(String[] args) {
+                int x, y, z;
+
+        // melakukan perulangan sebanyak x dan y kali
+        for (x = 0; x <= 1; x++){
+            for( y = 0; y <= 2; y++){
+                for( z = 0; z <= 3; z++){
+                System.out.format("Perulangan [x=%d, y=%d, z=%d] %n", x, y, z);
+            }
+        }
+
+    }
+    }
+    
+}
